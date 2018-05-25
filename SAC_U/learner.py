d1232a3180351e1310ff125f517052945cb9a205
gerben <gerry2211@gmail.com>
2018-05-25 20:55:20 +0200

    Finished most of the implementation of the Learner. Only gradient calculation left.

--- SAC_U/learner.py
+++ SAC_U/learner.py
@@ -9,8 +9,9 @@ import multiprocessing as mp
 
 class SacULearner(object):
     def __init__(self, parameter_server, policy_model, value_model, entropy_regularization, state_shape, action_space,
-                 n_tasks, buffer_size=-1, training_iterations=-1):
+                 n_tasks, learner_index, buffer_size=-1, training_iterations=-1):
 
+        self.learner_index = learner_index
         self.parameter_server = parameter_server
         self.policy_model = policy_model
         self.value_model = value_model
@@ -31,16 +32,38 @@ class SacULearner(object):
 
     def run(self):
         with tf.Session() as sess:
-            with tf.variable_scope("policy"):
-                policy = self.policy_model(self.task_id, self.state)
-            with tf.variable_scope("value"):
-                value = self.value_model(self.task_id, self.action, self.state)
+            with tf.variable_scope("current"):
+                with tf.variable_scope("policy"):
+                    policy = self.policy_model(self.task_id, self.state)
+                with tf.variable_scope("value"):
+                    value = self.value_model(self.task_id, self.action, self.state)
+            with tf.variable_scope("fixed"):
+                with tf.variable_scope("policy"):
+                    policy_fixed = self.policy_model(self.task_id, self.state)
+                with tf.variable_scope("value"):
+                    value_fixed = self.value_model(self.task_id, self.action, self.state)
             init = tf.global_variables_initializer()
             sess.run(init)
 
             n = 0
             while n < self.n_tasks or self.n_tasks == -1:
                 self.update_replay_buffer()
+                for k in range(1000):
+                    trajectory = self.replay_buffer[np.random.randint(0, len(self.replay_buffer)-1)]
+
+                    delta_phi = self.get_delta_phi(trajectory, sess)
+                    delta_theta = self.get_delta_theta(trajectory, sess)
+
+                    # The idea is that this will block until the parameter update is finished:
+                    self.parameter_server.put_gradients(self.learner_index, delta_phi, delta_theta)
+
+                    # Update the current parameters
+                    self.update_parameters(sess, both=False)
+
+                # Update all parameters
+                self.update_parameters(sess, both=True)
+
+                n += 1
 
     def action_to_one_hot(self, action_index):
         v = np.zeros((len(self.action_space,)))
@@ -54,3 +77,30 @@ class SacULearner(object):
             # Remove the first trajectory if the buffer is full:
             if self.buffer_size != -1 and len(self.replay_buffer) > self.buffer_size:
                 self.replay_buffer.pop(0)
+
+    def calculate_Q_return(self, trajectory, i=0):
+        pass
+
+    def update_parameters(self, sess, both=False):
+        variable_map = self.parameter_server.get_parameters()
+
+        # Construct a "query" of reassignments to run on the session
+        query = []
+        for var in tf.trainable_variables("current"):
+            if var.name.replace("current/", "") in variable_map:
+                query.append(tf.assign(var.name, variable_map[var.name]))
+        if both:
+            for var in tf.trainable_variables("fixed"):
+                if var.name.replace("fixed/", "") in variable_map:
+                    query.append(tf.assign(var.name, variable_map[var.name]))
+        sess.run(query)
+
+    def get_delta_phi(self, trajectory, sess):
+        # TODO: Generate the delta Phi
+        pass
+
+    def get_delta_theta(self, trajectory, sess):
+        # TODO: Generate the delta Theta
+        pass
+
+
