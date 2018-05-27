Fixed more bugs and implemented policy gradients.

SAC_U/learner.py
@@ -93,7 +93,8 @@ class SacULearner(object):
         """
         while self.trajectory_queue.qsize() > 0:
             print("Queue not empty: receiving trajectories")
-            self.replay_buffer.append(self.trajectory_queue.get())
+            trajectory = self.trajectory_queue.get()
+            self.replay_buffer.append(trajectory)
 
             # Remove the first trajectory if the buffer is full:
             print(self.buffer_size, len(self.replay_buffer))
@@ -139,14 +140,47 @@ class SacULearner(object):
         # TODO: Generate the delta Theta
         # TODO: Make it a beautiful map
 
-        vars = tf.trainable_variables("current/policy")
-        values = sess.run(vars)
+        policy, value, policy_fixed, value_fixed = parameters
 
-        ret = dict()
-        for var, val in zip(vars, values):
-            ret[var.name.replace("current/", "")] = val
+        states = np.stack([event[0] for event in trajectory[:]], axis=0)
+        # print("States: ", states)
+        task_ids = list(range(self.n_tasks))
+        values = np.zeros((self.n_tasks, len(states), len(self.action_space)))
 
-        return ret
+        for task_id in task_ids:
+            for action in range(len(self.action_space)):
+                tasks = np.array([task_id] * states.shape[0])
+                actions = np.repeat(np.expand_dims(self.action_to_one_hot(action), axis=0), states.shape[0], axis=0)
+
+                values[task_id, :, action] = np.reshape(sess.run(value, feed_dict=
+                {
+                    self.state: states,
+                    self.task_id: tasks,
+                    self.action: actions
+                }), (-1,))
+        print(values)
+        gradient_dict = dict()
+        for task_id in range(self.n_tasks):
+            task_policy_score = tf.reduce_sum(policy * values[task_id] + self.entropy_regularization * tf.log(policy))
+            query = []
+            keys = []
+            for variable in tf.trainable_variables("current/policy"):
+                query.append(tf.gradients(task_policy_score, variable))
+                keys.append(variable.name.replace("current/", ""))
+            gradients = sess.run(
+                query,
+                feed_dict={
+                    self.state: states,
+                    self.task_id: np.array([task_id]*states.shape[0])
+                })
+
+            for key, grad in zip(keys, gradients):
+                if key in gradient_dict:
+                    gradient_dict[key] += grad[0]
+                else:
+                    gradient_dict[key] = grad[0]
+        print(gradient_dict)
+        return gradient_dict
 
     def update_parameters(self, parameters):
         self.parameter_queue.put(parameters)

SAC_U/parameter_server.py
@@ -42,7 +42,10 @@ class SacUParameterServer(object):
                 value = self.value_model(self.task_id, self.action, self.state)
 
             optimizer = tf.train.AdamOptimizer(self.learning_rate)
-            _ = optimizer.apply_gradients(optimizer.compute_gradients(tf.concat([policy, value], axis=0), tf.trainable_variables()))
+
+            # Apparently this is necessary to initialize Adam:
+            _ = optimizer.apply_gradients(optimizer.compute_gradients(policy, tf.trainable_variables()))
+            _ = optimizer.apply_gradients(optimizer.compute_gradients(value, tf.trainable_variables()))
             init = tf.global_variables_initializer()
             sess.run(init)
             self.update_parameter_variable(sess)

SAC_U/sac_u.py
@@ -69,9 +69,9 @@ if __name__ == "__main__":
     def value_model(t_id, action, x):
         x = tf.concat([x, action], axis=1)
         x = ks.layers.Dense(10, activation='elu')(x)
-        return ks.layers.Dense(len(mock_env.action_space), activation='linear')(x)
+        return ks.layers.Dense(1, activation='linear')(x)
 
 
     env = lambda: mock_env.MockEnv()
-    sac_u = SacU(policy_model, value_model, env, mock_env.state_shape, mock_env.action_space, mock_env.n_tasks, 2, buffer_size=10)
+    sac_u = SacU(policy_model, value_model, env, mock_env.state_shape, mock_env.action_space, mock_env.n_tasks, 2, buffer_size=1000)
     sac_u.run()

environments/mock_env.py
@@ -10,8 +10,8 @@ class MockEnv(object):
         pass
 
     def reset(self):
-        return np.zeros(state_shape)
+        return np.random.normal(0, 1, state_shape)
 
     def step(self, action):
-        return np.zeros(state_shape), np.random.normal(0, 1, (n_tasks,))
+        return np.random.normal(0, 1, state_shape), np.random.normal(0, 1, (n_tasks,))
 