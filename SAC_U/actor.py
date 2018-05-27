Actually tested the code with the weights as gradient (because the losses are not defined yet). Fixed a lot of practical issues with inter-process communication. The parameters now seem to be updated correctly.

SAC_U/actor.py
@@ -3,7 +3,7 @@
 """
 import tensorflow as tf
 import numpy as np
-
+import multiprocessing as mp
 
 class SacUActor(object):
     def __init__(self, environment, n_trajectories, max_steps, scheduler_period, state_shape, action_space, n_tasks,
@@ -26,11 +26,12 @@ class SacUActor(object):
         self.state_shape = state_shape
         self.action_space = action_space
         self.n_tasks = n_tasks
-        self.state = tf.placeholder(tf.float32, state_shape)
-        self.task_id = tf.placeholder(tf.int32)
+        self.state = tf.placeholder(tf.float32, (None,) + state_shape)
+        self.task_id = tf.placeholder(tf.int32, (None,))
         self.env = environment
         self.learner = learner
         self.parameter_server = parameter_server
+        self.parameter_queue = mp.Queue()
         # Since this is SAC_U, there is no Q-table for scheduling
 
     def run(self):
@@ -47,7 +48,7 @@ class SacUActor(object):
                 s = self.env.reset()
                 trajectory = []
                 while n < self.n_trajectories:
-                    self.update_parameters(sess)
+                    self.update_local_parameters(sess)
 
                     # Collect a new trajectory from the environment
                     for t in range(self.max_steps):
@@ -56,7 +57,7 @@ class SacUActor(object):
                             task_id = np.random.randint(0, self.n_tasks)
                         action_dist = sess.run(
                             policy,
-                            feed_dict={self.state: np.expand_dims(s, axis=0), self.task_id: task_id}
+                            feed_dict={self.state: np.expand_dims(s, axis=0), self.task_id: np.array([task_id])}
                         )[0]
                         a_i = self.sample_index(action_dist)
                         a = self.action_space[a_i]
@@ -80,12 +81,18 @@ class SacUActor(object):
         raise ValueError("Expected cumulative probability of 1, got %s. Failed to sample from distribution %s." % (
             np.sum(distribution), distribution))
 
-    def update_parameters(self, sess):
+    def update_local_parameters(self, sess):
         """
-        Requests fresh parameters from the parameter server applies these new parameters
+        Gets fresh parameters from the parameter server applies these new parameters
         :param sess: The TensorFlow session
         """
-        variable_map = self.parameter_server.get_parameters()
+
+        if self.parameter_queue.empty():
+            return
+        variable_map = self.parameter_queue.get()
+        while not self.parameter_queue.empty():
+            variable_map = self.parameter_queue.get()
+
 
         # Construct a "query" of reassignments to run on the session
         query = []
@@ -96,3 +103,6 @@ class SacUActor(object):
 
     def send_trajectory(self, trajectory):
         self.learner.add_trajectory(trajectory)
+
+    def update_parameters(self, parameters):
+        self.parameter_queue.put(parameters)

SAC_U/learner.py
@@ -2,6 +2,8 @@
     The Learner in the SAC-U algorithm collects trajectories in the replay memory and computes gradients
     from the replay memory. These gradients are then sent to a central parameter server.
 """
+import time
+
 import tensorflow as tf
 import numpy as np
 import multiprocessing as mp
@@ -22,10 +24,12 @@ class SacULearner(object):
         self.state_shape = state_shape
         self.action_space = action_space
         self.n_tasks = n_tasks
-        self.state = tf.placeholder(tf.float32, state_shape)
-        self.task_id = tf.placeholder(tf.int32)
-        self.action = tf.placeholder(tf.float32, shape=(len(action_space,)))
+        self.n_tasks = n_tasks
+        self.state = tf.placeholder(tf.float32, (None,) + state_shape)
+        self.task_id = tf.placeholder(tf.int32, (None,))
+        self.action = tf.placeholder(tf.float32, shape=(None, len(action_space,)))
         self.replay_buffer = []
+        self.parameter_queue = mp.Queue()
 
     def add_trajectory(self, trajectory):
         self.trajectory_queue.put(trajectory)
@@ -47,8 +51,15 @@ class SacULearner(object):
             sess.run(init)
 
             n = 0
+
+            # Wait until the first trajectories appear if the buffer is empty
+            while len(self.replay_buffer) <= 1:
+                self.update_replay_buffer()
+                time.sleep(0.1)
+
             while n < self.n_tasks or self.n_tasks == -1:
                 self.update_replay_buffer()
+
                 for k in range(1000):
                     trajectory = self.replay_buffer[np.random.randint(0, len(self.replay_buffer)-1)]
 
@@ -59,10 +70,11 @@ class SacULearner(object):
                     self.parameter_server.put_gradients(self.learner_index, delta_phi, delta_theta)
 
                     # Update the current parameters
-                    self.update_parameters(sess, both=False)
+                    if k != 999:
+                        self.update_local_parameters(sess, both=False)
 
                 # Update all parameters
-                self.update_parameters(sess, both=True)
+                self.update_local_parameters(sess, both=True)
 
                 n += 1
 
@@ -76,34 +88,57 @@ class SacULearner(object):
             self.replay_buffer.append(self.trajectory_queue.get(0))
 
             # Remove the first trajectory if the buffer is full:
+            print(self.buffer_size, len(self.replay_buffer))
             if self.buffer_size != -1 and len(self.replay_buffer) > self.buffer_size:
                 self.replay_buffer.pop(0)
 
     def calculate_Q_return(self, trajectory, i=0):
+        # TODO: Implement
         pass
 
-    def update_parameters(self, sess, both=False):
-        variable_map = self.parameter_server.get_parameters()
+    def update_local_parameters(self, sess, both=False):
+        if self.parameter_queue.empty():
+            return
+        variable_map = self.parameter_queue.get()
+        while not self.parameter_queue.empty():
+            variable_map = self.parameter_queue.get()
 
         # Construct a "query" of reassignments to run on the session
         query = []
         for var in tf.trainable_variables("current"):
             if var.name.replace("current/", "") in variable_map:
-                query.append(tf.assign(var.name, variable_map[var.name]))
+                query.append(tf.assign(var, variable_map[var.name.replace("current/", "")]))
         if both:
             for var in tf.trainable_variables("fixed"):
                 if var.name.replace("fixed/", "") in variable_map:
-                    query.append(tf.assign(var.name, variable_map[var.name]))
+                    query.append(tf.assign(var, variable_map[var.name.replace("fixed/", "")]))
         sess.run(query)
 
-    def get_delta_phi(self, trajectory, parameters, sess):
+    def get_delta_phi(self, trajectory, parameters, sess) -> dict:
         # TODO: Generate the delta Phi
-        # TODO: Make it a beautiful map
-        return dict()
+        # Stub implementation:
+        vars = tf.trainable_variables("current/value")
+        values = sess.run(vars)
 
-    def get_delta_theta(self, trajectory, parameters, sess):
+        ret = dict()
+        for var, val in zip(vars, values):
+            ret[var.name.replace("current/", "")] = val
+
+        return ret
+
+    def get_delta_theta(self, trajectory, parameters, sess) -> dict:
         # TODO: Generate the delta Theta
         # TODO: Make it a beautiful map
-        return dict()
 
+        vars = tf.trainable_variables("current/policy")
+        values = sess.run(vars)
+
+        ret = dict()
+        for var, val in zip(vars, values):
+            ret[var.name.replace("current/", "")] = val
+
+        return ret
+
+    def update_parameters(self, parameters):
+        self.parameter_queue.put(parameters)
 

SAC_U/parameter_server.py
@@ -12,16 +12,16 @@ class SacUParameterServer(object):
         self.gradients_to_average = gradients_to_average
         self.value_model = value_model
         self.policy_model = policy_model
+        self.actors = []
         self.learners = []
-        self.learner_barriers = []
-        self.learner_waiting = []
         self.id = 0
         self.gradient_queue = mp.Queue()
 
-        self.state = tf.placeholder(tf.float32, state_shape)
-        self.task_id = tf.placeholder(tf.int32)
-        self.action = tf.placeholder(tf.float32, shape=(len(action_space, )))
-        self.parameters = None
+        self.state = tf.placeholder(tf.float32, (None,) + state_shape)
+        self.task_id = tf.placeholder(tf.int32, (None,))
+        self.action = tf.placeholder(tf.float32, shape=(None, len(action_space,)))
+        self.manager = mp.Manager()
+        self.parameters = self.manager.dict()
 
     def add_learner(self, learner):
         """
@@ -32,9 +32,8 @@ class SacUParameterServer(object):
         self.id += 1
         self.learners.append(learner)
 
-        # Add a barrier to hold the learner until parameters are updated
-        self.learner_barriers.append(mp.Barrier(2))
-        self.learner_waiting.append(False)
+    def add_actor(self, actor):
+        self.actors.append(actor)
 
     def run(self):
         with tf.Session() as sess:
@@ -42,7 +41,9 @@ class SacUParameterServer(object):
                 policy = self.policy_model(self.task_id, self.state)
             with tf.variable_scope("value"):
                 value = self.value_model(self.task_id, self.action, self.state)
+
             optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            _ = optimizer.apply_gradients(optimizer.compute_gradients(tf.concat([policy, value], axis=0), tf.trainable_variables()))
             init = tf.global_variables_initializer()
             sess.run(init)
             self.update_parameter_variable(sess)
@@ -53,8 +54,13 @@ class SacUParameterServer(object):
                     delta_theta, delta_phi = self.get_gradients()
                     d_theta.append(delta_theta)
                     d_phi.append(delta_phi)
-                optimizer.apply_gradients(self.get_grads(d_phi))
-                optimizer.apply_gradients(self.get_grads(d_theta))
+                    n += 1
+                sess.run([
+                    optimizer.apply_gradients(self.get_grads(d_phi)),
+                    optimizer.apply_gradients(self.get_grads(d_theta))
+                ])
+
+                self.update_parameter_variable(sess)
 
     def get_grads(self, gradients: list) -> list:
         """
@@ -62,7 +68,6 @@ class SacUParameterServer(object):
         :param gradients: A list of dicts of the form var_name -> gradient
         :return: Averaged gradients in the form [(grad_1, var_1), (grad_2, var_2), ...]
         """
-
         output = []
         for variable in tf.trainable_variables():
             sum_gradients = None
@@ -73,6 +78,8 @@ class SacUParameterServer(object):
                         sum_gradients = gradient
                     else:
                         sum_gradients += gradient
+            if sum_gradients is None:
+                continue
             sum_gradients /= len(gradients)
             output.append((sum_gradients, variable))
         return output
@@ -85,23 +92,20 @@ class SacUParameterServer(object):
         for var, val in zip(vars, vals):
             params[var.name] = val
         self.parameters = params
+        print(params)
 
-        # Unlock the waiting learners
-        for barrier, waiting in zip(self.learner_barriers, self.learner_waiting):
-            if waiting:
-                barrier.wait()
+        for actor in self.actors:
+            actor.update_parameters(self.parameters)
+
+        for learner in self.learners:
+            learner.update_parameters(self.parameters)
+
 
     def get_gradients(self):
         return self.gradient_queue.get()
 
     def put_gradients(self, index, delta_phi, delta_theta):
         # This method will be executed by learners!
-        self.learner_waiting[index] = True
         self.gradient_queue.put((delta_phi, delta_theta))
 
-        # The Learner thread will hold here until gradients are updated
-        self.learner_barriers[index].wait()
 
-        # The learner will now reset its status and resume learning
-        self.learner_barriers[index].reset()
-        self.learner_waiting[index] = False

SAC_U/sac_u.py
@@ -1,21 +1,23 @@
 """
     The central class that starts everything. It will manage the startup of all the other classes.
 """
+import traceback
 
 from SAC_U.parameter_server import SacUParameterServer
 from SAC_U.learner import SacULearner
 from SAC_U.actor import SacUActor
+import multiprocessing as mp
 
 
 class SacU(object):
-    def __init__(self, policy_model, value_model, environment, state_shape, action_space, n_tasks, n_learners,
+    def __init__(self, policy_model, value_model, environment_creator, state_shape, action_space, n_tasks, n_learners,
                  learning_rate=0.0001, averaged_gradients=None, entropy_regularization_factor=0.1, n_trajectories=4,
                  max_steps = 4000, scheduler_period=150):
         self.entropy_regularization_factor = entropy_regularization_factor
         self.n_tasks = n_tasks
         self.action_space = action_space
         self.state_shape = state_shape
-        self.environment = environment
+        self.environment_creator = environment_creator
         self.policy_model = policy_model
         self.value_model = value_model
         self.learning_rate = learning_rate
@@ -29,12 +31,44 @@ class SacU(object):
         for i in range(n_learners):
             learner = SacULearner(self.parameter_server, policy_model, value_model, entropy_regularization_factor,
                                   state_shape, action_space, n_tasks)
-            actor = SacUActor(environment, n_trajectories, max_steps, scheduler_period, state_shape, action_space,
+            actor = SacUActor(environment_creator(), n_trajectories, max_steps, scheduler_period, state_shape, action_space,
                               n_tasks, policy_model, learner, self.parameter_server)
             self.actors.append(actor)
             self.learners.append(learner)
             self.parameter_server.add_learner(learner)
 
     def run(self):
-        # TODO: Run all threads. Run the parameter server on this thread
-        pass
+        processes = []
+        for learner, actor in zip(self.learners, self.actors):
+            p1 = mp.Process(target=learner.run)
+            p2 = mp.Process(target=actor.run)
+            processes.append(p1)
+            processes.append(p2)
+            p1.start()
+            p2.start()
+        try:
+            self.parameter_server.run()
+        except Exception:
+            traceback.print_exc()
+            for process in processes:
+                process.terminate()
+
+if __name__ == "__main__":
+    import tensorflow as tf
+    ks = tf.keras
+    import environments.mock_env as mock_env
+
+    def policy_model(t_id, x):
+        x = ks.layers.Dense(10, activation='elu')(x)
+        return ks.layers.Dense(len(mock_env.action_space), activation=ks.activations.softmax)(x)
+
+
+    def value_model(t_id, action, x):
+        x = tf.concat([x, action], axis=1)
+        x = ks.layers.Dense(10, activation='elu')(x)
+        return ks.layers.Dense(len(mock_env.action_space), activation='linear')(x)
+
+
+    env = lambda: mock_env.MockEnv()
+    sac_u = SacU(policy_model, value_model, env, mock_env.state_shape, mock_env.action_space, mock_env.n_tasks, 2)
+    sac_u.run()